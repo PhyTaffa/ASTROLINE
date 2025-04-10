20118190880591039a909897659f919958f83d50
SILVERU-GOLDERU <rorgzen@gmail.com>
2025-04-10 12:47:19 +0100

    Ui world rotates

--- Assets/Scripts/Miguel/Menu/InteractiveWorld.cs
+++ Assets/Scripts/Miguel/Menu/InteractiveWorld.cs
@@ -2,17 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractiveWorld : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class InteractiveWorld : MonoBehaviour {
+    
+    public float rotationSpeed = 5f;  
+    
+    private bool isDragging = false;
+    private bool rotateMode = false;
+    private Quaternion initialRotation;
+
+    void Start(){
+        initialRotation = transform.rotation;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
+        if (Input.GetKeyDown(KeyCode.R)){
+            
+            //switches bool
+            rotateMode = !rotateMode;                       
+            
+            //checks if false
+            if (rotateMode){  
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+                
+            }else{
+                
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                transform.rotation = initialRotation;
+            }                                
+              
+        }
+
+        //checks in true
+        if (rotateMode){
+
+            if (Input.GetMouseButtonDown(0)){
+                
+                isDragging = true; 
+            }
+
+            if (Input.GetMouseButtonUp(0)){
+                
+                isDragging = false;
+            }
+
+
+            if (isDragging){
+
+                float mouseX = Input.GetAxis("Mouse X");
+                float mouseY = Input.GetAxis("Mouse Y");
+
+                Vector3 rotationVector = new Vector3(mouseY, -mouseX, 0) * rotationSpeed;
+
+                transform.Rotate(rotationVector, Space.World);
+            }
+        }
     }
 }
