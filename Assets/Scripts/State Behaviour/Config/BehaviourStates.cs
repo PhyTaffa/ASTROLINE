35a0990bf1f88dd063530526fe77dd2927618db0
PhyTaffa <filippotaffarello@gmail.com>
2025-04-22 03:35:10 +0100

    simple implementation

---
+++ Assets/Scripts/State Behaviour/Config/BehaviourStates.cs
@@ -0,0 +1,10 @@
+// Use this file to declare all your states for a given enumeration or statemachine
+public enum EFaunaState
+{
+    Invalid = -1,
+    Idle,
+    Patrolling,
+    Chasing,
+    Reacting,
+}
+
