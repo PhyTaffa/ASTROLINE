f7d13e1dc7d4c3ab9a7e3d230e0fc89fc689e7e1
PhyTaffa <filippotaffarello@gmail.com>
2025-04-26 19:36:10 +0100

    final scnee thisdxgacvxkjz

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -6,17 +6,25 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private GameObject vCamGO;
     [SerializeField] private CinemachineVirtualCamera cinemachineCamera;
+    private Quaternion vcamRot;
+    
     void Start()
     {
-        //cinemachineCamera = Camera.main.GetComponent<CinemachineVirtualCamera>();
+        //vCamGO = cinemachineCamera.GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = cinemachineCamera.transform.position;
-        this.transform.rotation = cinemachineCamera.transform.rotation;
-        
+        Vector3 euler = vCamGO.transform.rotation.eulerAngles; 
+        vcamRot = vCamGO.transform.rotation;
+        Debug.Log(euler.x);
+        // vcamRot.y = 0;
+        // vcamRot.z = 0;
+        // vcamRot.x *= -1;
+        //this.transform.rotation =  Quaternion.Euler(-euler.x, 0f, 0f);
+        //this.transform.rotation.x = vcamRot.x * -1;
     }
 }
