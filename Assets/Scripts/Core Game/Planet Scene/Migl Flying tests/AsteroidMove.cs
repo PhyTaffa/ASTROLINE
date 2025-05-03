d4d0e16da3c7367bc7661d8607933c903d1bf2f7
SILVERU-GOLDERU <rorgzen@gmail.com>
2025-05-03 21:24:13 +0100

    Asteroids

---
+++ Assets/Scripts/Core Game/Planet Scene/Migl Flying tests/AsteroidMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMove : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 60f;
+    private float rotateSpeed = 500f;
+
+    private Vector3 driftDirection = Vector3.forward;
+    private Rigidbody rb;
+    private Transform tf;
+    private WorldGravity worldGravity;
+
+    private void Start() {
+
+        rb = GetComponent<Rigidbody>();
+        tf = transform;
+    }
+    
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + transform.TransformDirection(driftDirection * moveSpeed * Time.deltaTime));
+    }
+
+
+}
