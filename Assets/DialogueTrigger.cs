a3ad160711620a11124a930a6de1c237d6cdbf26
li1ra7 <sezimaabdigaparova@gmail.com>
2025-05-27 16:31:36 +0100

    Update DialogueTrigger.cs

--- Assets/DialogueTrigger.cs
+++ Assets/DialogueTrigger.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour
+public class DiLOGTRIGGER : MonoBehaviour
 {
     [SerializeField] private string tagToCheck = "Player";
-    private bool hasBeenTriggeredAlready = false;
-
     [SerializeField] private string textToDisplay = "Default dialogue text";
     [SerializeField] private float displayDuration = 3f;
 
     public GameObject dialoguePanel;
     public TextMeshProUGUI dialogueText;
 
+    private bool hasBeenTriggeredAlready = false;
+    private CameraManager camManager;
+
+    private void Start()
+    {
+        camManager = FindObjectOfType<CameraManager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (!hasBeenTriggeredAlready && other.CompareTag(tagToCheck))
         {
+            // Only trigger if we're in third-person view
+            if (camManager != null && camManager.IsFirstPersonActive()) return;
+
             hasBeenTriggeredAlready = true;
             ShowDialogue();
         }
@@ -24,9 +33,12 @@ public class DialogueTrigger : MonoBehaviour
 
     private void ShowDialogue()
     {
-        dialogueText.text = textToDisplay;
-        dialoguePanel.SetActive(true);
-        StartCoroutine(TimedWindow(displayDuration));
+        if (dialoguePanel != null && dialogueText != null)
+        {
+            dialogueText.text = textToDisplay;
+            dialoguePanel.SetActive(true);
+            StartCoroutine(TimedWindow(displayDuration));
+        }
     }
 
     private IEnumerator TimedWindow(float timer)
