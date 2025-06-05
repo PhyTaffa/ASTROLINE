74b4becb34b8f54d5e3a1c46e4202710e39ed33b
PhyTaffa <filippotaffarello@gmail.com>
2025-06-05 18:41:45 +0100

    Char wlaking in train

--- Assets/Scripts/_Ship Scene/ShipPlayerController.cs
+++ Assets/Scripts/_Ship Scene/ShipPlayerController.cs
@@ -5,9 +5,12 @@ public class ShipPlayerController : MonoBehaviour{
     private float rotationSpeed = 960f;
 
     private Rigidbody rb;
+    [SerializeField] private PlayerAnimator playerAnim;
 
     void Awake(){
         rb = GetComponent<Rigidbody>();
+        
+        playerAnim.SetAnimationState(EPlayerAnimatorStates.Idle);
     }
 
     void FixedUpdate(){
@@ -17,9 +20,13 @@ public class ShipPlayerController : MonoBehaviour{
 
         // desired movement direction
         Vector3 dir = new Vector3(h, 0f, v).normalized;
-        if (dir.sqrMagnitude < 0.01f){
+        if (dir.sqrMagnitude < 0.01f)
+        {
+            playerAnim.SetAnimationState(EPlayerAnimatorStates.Idle);    
             return; 
         }
+        
+        playerAnim.SetAnimationState(EPlayerAnimatorStates.Walk);  
         
         float targetYaw = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
         float newYaw = Mathf.MoveTowardsAngle(rb.rotation.eulerAngles.y, targetYaw, rotationSpeed * Time.fixedDeltaTime);
